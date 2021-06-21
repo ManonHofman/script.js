c17e559a01ac523d1db959941818cbb407ccaede
ManonHofman <84331614+ManonHofman@users.noreply.github.com>
2021-06-21 16:32:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Hallo Winc Academy studenten")
+// Dit is een grote som
+let multiplication = 12309411 * 1823794;
+console.log("multiplication:", multiplication);
+let division = 637263 / 54;
+console.log("division:", division);
+/* Hallo  */
